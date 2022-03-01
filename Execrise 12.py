80bcb57a05fad20902d1d239f55aad903854a274
Kgee321 <geek@middleton.school.nz>
2022-03-02 10:11:53 +1300

    Good

--- Execrise 12.py
+++ Execrise 12.py
@@ -8,11 +8,16 @@ YAY
 
 def end(cost, ad, st, ch, gv):
     total_ticket = ad + st + ch + gv
+    print("="*50)
     print(f"The total tickets brought today was {total_ticket}")
     print(f"""This was made up of: 
-{ad} for Adults,
+{ad} for Adults ,
 {st} for Students
-{}""")
+{ch} for Children
+{gv} for Gift Vouchers """)
+    print(f"Sale for today comes to {cost:,.2f}")
+    print("="*50)
+
 
 adult = 0
 student = 0
@@ -20,15 +25,15 @@ child = 0
 gift = 0
 total = 0
 
-yes_or_no = input("Do you want to buy a ticket? (Y/N)").lower()
+yes_or_no = input("Do you want to buy a ticket? (Y/N) ").lower()
 while yes_or_no == "y":
     ticket = input("""What type of ticket do you want to buy?
-    A for Adult, or
-    S for Student, or 
-    C for Child, or 
-    G for Gift voucher
-    """).upper()
-    confirm = input(f"Confirm purchase of {ticket} ticket? (Y/N)").lower()
+A for Adult, or
+S for Student, or 
+C for Child, or 
+G for Gift voucher
+""").upper()
+    confirm = input(f"Confirm purchase of {ticket} ticket? (Y/N) ").lower()
     if confirm == "y":
         if ticket == "A":
             total += 12.5
@@ -39,8 +44,10 @@ while yes_or_no == "y":
         if ticket == "C":
             total += 7
             child += 1
-        if ticket = "G":
+        if ticket == "G":
             gift += 1
-    else:
-        print(end(total, adult, student, child, gift))
+    print()
+    yes_or_no = input("Do you want to buy another ticket? (Y/N) ").lower()
 
+if yes_or_no == "n":
+    end(total, adult, student, child, gift)
--- Execrise 9 2.py
+++ Execrise 9 2.py
@@ -1,10 +1,12 @@
 def longest_word(string_list):
-    total = ''
+    total = ['']
     for i in range(len(string_list)):
-        if len(total) < len(string_list[i]):
-            total = string_list[i]
+        if len(total[0]) < len(string_list[i]):
+            total = [string_list[i]]
+        elif len(total[0]) == len(string_list[i]):
+            total.append(string_list[i])
     return total
 
 
 long_list = input("Enter list: ").split()
-print(longest_word(long_list))
+print(f"longest words in list is {longest_word(long_list)}")
