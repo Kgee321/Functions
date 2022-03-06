2319725edfe72c58e6b180bf332e0742dcf62de1
Kgee321 <geek@middleton.school.nz>
2022-03-07 12:41:25 +1300

    v2

--- Execrise 12.py
+++ Execrise 12.py
@@ -1,31 +1,54 @@
+"""Program for selling Movie Ticket at a cinema -- V2 to keep track of tickets sold
+ Created by Katelyn Gee
+ 7/03/2022
+ """
+
+
 def main_function():
+
+    # End function -- shows total sales
     def end(cost, ad, st, ch, gv):
         total_ticket = ad + st + ch + gv
-        print("="*50)
-        print(f"The total tickets brought today was {total_ticket}")
+        print("=" * 50)
+        print(f"The total tickets sold today was {total_ticket}")
         print(f"""This was made up of: 
     {ad} for Adults ,
     {st} for Students
     {ch} for Children
     {gv} for Gift Vouchers """)
-        print(f"Sale for today comes to ${cost:,.2f}")
-        print("="*50)
+        print(f"Sales for the day come to ${cost:,.2f}")
+        print("=" * 50)
 
+    # Starting amount of tickets for each possible ticket -- Always starts with 0
     adult = 0
     student = 0
     child = 0
     gift = 0
     total = 0
 
-    yes_or_no = input("Do you want to buy a ticket? (Y/N) ").lower()
+    yes_or_no = input("Do you want to sell a ticket? (Y/N) ").lower()
     while yes_or_no == "y":
-        ticket = input("""What type of ticket do you want to buy?
-    A for Adult, or
-    S for Student, or 
-    C for Child, or 
-    G for Gift voucher
-    """).upper()
-        number = int(input(f"How many {ticket} ticket's do you want to buy? "))
+        ticket = input("""What type of ticket do you want?
+A for Adult, or
+S for Student, or 
+C for Child, or 
+G for Gift voucher
+""").upper()
+
+        # if they put in the wrong type of ticket
+        if ticket != "A" and ticket != "S" and ticket != "C" and ticket != "G":
+            print("""That was an incorrect ticket so it has been cancelled, try again using either 
+A for Adult, or
+S for Student, or 
+C for Child, or 
+G for Gift voucher""")
+            print()
+            main_function()
+
+        # number of tickets they are selling of that type of ticket
+        number = int(input(f"How many {ticket} ticket's do you want to sell? "))
+
+        # confirming if they want that ticket
         confirm = input(f"Confirm purchase of {number} {ticket} ticket/s? (Y/N) ").lower()
         if confirm == "y":
             if ticket == "A":
@@ -39,12 +62,22 @@ def main_function():
                 child += number
             if ticket == "G":
                 gift += number
+        elif confirm == "n":
+            print("Ok, Ticket as been cancelled")
+        else:
+            print("That was wrong input, ticket has been cancelled, either but Y for yes or N for no:")
+            main_function()
         print()
-        yes_or_no = input("Do you want to buy another ticket? (Y/N) ").lower()
+        yes_or_no = input("Do you want to sell another ticket? (Y/N) ").lower()
 
     if yes_or_no == "n":
         end(total, adult, student, child, gift)
+    elif yes_or_no != "n" or "y":
+        print("That was wrong input, either but Y for yes or N for no:")
+        print()
+        main_function()
 
 
+# Main function
 print("*************** Movie Ticket buying **********************")
 main_function()
